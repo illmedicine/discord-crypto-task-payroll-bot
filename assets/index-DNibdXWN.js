bee8b1f64681d925d732ba3871fcbd180ca36970
illmedicine <illmedicine@users.noreply.github.com>
2026-05-03 20:07:10 +0000

    deploy: 0816e66da863fff504d483f5e30d02f50879df59

---
+++ assets/index-DNibdXWN.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DSN6kwi7.js","assets/index-D8gQjEOS.js"])))=>i.map(i=>d[i]);
+import{_ as e}from"./index-BJMvmM6M.js";import{r as p}from"./index-D8gQjEOS.js";const _=p("App",{web:()=>e(()=>import("./web-DSN6kwi7.js"),__vite__mapDeps([0,1])).then(r=>new r.AppWeb)});export{_ as App};
